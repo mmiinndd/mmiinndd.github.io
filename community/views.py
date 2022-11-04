c2a9e1ae63702c0d27669314b1a069055811ce22
mmiinndd <minju8633@naver.com>
2022-11-04 17:52:08 +0900

    1111

--- community/views.py
+++ community/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
+class PostList(ListView):
+    model = Post
+    ordering = 'pk'
+
 
 # Create your views here.
