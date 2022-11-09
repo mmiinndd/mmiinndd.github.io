1656ece199c25f4dfefaccda26cf8914b54ce6ef
mmiinndd <minju8633@naver.com>
2022-11-09 20:37:13 +0900

    모듈화

--- WINBRD_prj/urls.py
+++ WINBRD_prj/urls.py
@@ -11,13 +11,17 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('community/', include('community.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('community/', include('community.urls')),
     path('admin/', admin.site.urls),
     path('', include('single_pages.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
