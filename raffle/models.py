8af45586c5fd1551409d8da55c7044282c239431
mmiinndd <minju8633@naver.com>
2022-11-29 19:53:49 +0900

    하기싫다

--- raffle/models.py
+++ raffle/models.py
@@ -4,11 +4,13 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='raffle/images/%Y/%m/%d/', blank=True)
     create_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'[self.title]{self.title}'
-
+    def get_absolute_url(self):
+        return f'/raffle/{self.pk}/'
 
 # Create your models here.
